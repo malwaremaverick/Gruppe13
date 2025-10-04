e2b4cf0673fcca55ebe53746f6458548fd1e7a0a
Vincent Mukendi <vnmukend@hiof.no>
2025-10-04 18:49:58 +0200

    lagt til mode og modemapper (F4)

---
+++ src/main/no/gruppe13/kollektiv/shared/Mode.java
@@ -0,0 +1,27 @@
+package no.gruppe13.kollektiv.shared;
+
+// Typer transport vi støtter 
+
+
+enum Mode {
+    // Navn i koden            // Navn som kan vises i UI
+    BUS("Buss"),
+    TRAIN("Tog"),
+    TRAM("Trikk"),
+    METRO("T-bane"),
+    FERRY("Ferge"),
+    WALK("Gange"),     // brukes om man må gå for å bytte mellom transportmidler/holdeplass. skal ofet være alltid tillatt.  
+    OTHER("Annet");    // ukjent type fra API eller noe vi ikke har mappet enda
+
+    private final String displayName;
+
+    Mode(String displayName) {
+        this.displayName = displayName;
+    }
+
+    // Tekst vi kan vise i UI senere
+    public String displayName() {
+        return displayName;
+    }
+
+}
---
+++ src/main/no/gruppe13/kollektiv/shared/ModeMapper.java
@@ -0,0 +1,26 @@
+package no.gruppe13.kollektiv.shared;
+
+
+class ModeMapper {
+    private ModeMapper() {}
+
+    public static Mode fromApi(String raw) {
+        if (raw == null) return Mode.OTHER;
+        String s = raw.trim().toLowerCase();
+
+        // oversetter ulike typer navn/inputs til samme mode 
+        switch (s) {
+            case "bus": return Mode.BUS;
+            case "train":
+            case "rail": return Mode.TRAIN;
+            case "tram": return Mode.TRAM;
+            case "metro":
+            case "subway": return Mode.METRO;
+            case "ferry":
+            case "boat": return Mode.FERRY;
+            case "walk":
+            case "foot": return Mode.WALK;
+            default: return Mode.OTHER; // ukjent type, så fall tilbake
+        }
+    }
+}
