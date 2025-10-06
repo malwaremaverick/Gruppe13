a6763635628861759a85732a25865b214ad8667a
Vincent Mukendi <vnmukend@hiof.no>
2025-10-06 16:23:32 +0200

    kobler filterservice til departures/journey services

---
+++ src/main/no/gruppe13/kollektiv/service/DeparturesService.java
@@ -0,0 +1,5 @@
+// etter at man har hentet alle avganger:
+
+var fs = ;
+var filtered = FilterService.filterDepartures(allDepartures, fs);
+return filtered;
---
+++ src/main/no/gruppe13/kollektiv/service/JourneyService.java
@@ -0,0 +1,5 @@
+// etter at man har søkt ruter:
+
+var fs = ;
+var filtered = FilterService.filterJourneys(allJourneys, fs);
+return filtered;
